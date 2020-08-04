442341e2c907131ad6571f2e42d14f5eb0e3415b
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-08-04 11:01:59 -0400

    added clickable movie and TV components

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ButtonBases from "./CustomButtons"
 
 function Welcome() {
   return (
@@ -6,6 +7,7 @@ function Welcome() {
       <h1>Welcome to Dan's Movie Database!</h1>
       <h2>It's really just a worse IMDb</h2>
       <h3>But have fun searching movies and TV shows!</h3>
+      <ButtonBases />
     </div>
   )
 }
