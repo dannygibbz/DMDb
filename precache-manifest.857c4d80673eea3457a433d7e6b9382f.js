e70764b3352f9004c8e66a6528cafb4d10fcba18
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-08-04 12:48:51 -0400

    Updates

---
+++ precache-manifest.857c4d80673eea3457a433d7e6b9382f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07164d3d964d2ffe5155eedc3c716e34",
+    "url": "/DMDb/index.html"
+  },
+  {
+    "revision": "193a0efb95f045003203",
+    "url": "/DMDb/static/css/main.d59fcfdd.chunk.css"
+  },
+  {
+    "revision": "0969db73f7b4ebe521cb",
+    "url": "/DMDb/static/js/2.100e1c98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DMDb/static/js/2.100e1c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193a0efb95f045003203",
+    "url": "/DMDb/static/js/main.f8ca1081.chunk.js"
+  },
+  {
+    "revision": "5cce3dc91d4bf05a080c",
+    "url": "/DMDb/static/js/runtime-main.3db93e00.js"
+  },
+  {
+    "revision": "d0f84823d1790ffe1161d13aaf94aee4",
+    "url": "/DMDb/static/media/yarn.d0f84823.jpg"
+  }
+]);
